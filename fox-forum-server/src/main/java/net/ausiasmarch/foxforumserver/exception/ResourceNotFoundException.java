6899288beb668f5c67c92328235536a53b93d6ad
Eva Lara Velarte <evalaravel@gmail.com>
2023-10-05 14:02:12 +0200

    Subo proyecto SpringBoot

---
+++ fox-forum-server/src/main/java/net/ausiasmarch/foxforumserver/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package net.ausiasmarch.foxforumserver.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+}
